64dbcbfea0e3ffeb51b142e7f598b4702f2e3528
Jeremiah England <englandtuning@gmail.com>
2023-09-23 20:55:32 -0400

    fix: Add debug logs and handle slight offsets in has_event

--- src/aw_watcher_ask_away/core.py
+++ src/aw_watcher_ask_away/core.py
@@ -1,6 +1,7 @@
 # ruff: noqa: EM101, EM102
 import datetime
 import logging
+from collections import deque
 from functools import cached_property
 from itertools import pairwise
 from typing import Any
@@ -59,28 +60,48 @@ class AWAskAwayState:
             # TODO: Look into why aw-watcher-afk uses queued=True here.
             client.create_bucket(self.bucket_id, event_type="afktask")
 
-        self.recent_events = {
-            (event.timestamp, event.duration): event for event in client.get_events(self.bucket_id, limit=10)
-        }
+        self.recent_events: deque[aw_core.Event] = deque(maxlen=10)
+        self.recent_events.extend(aw_transform.sort_by_timestamp(client.get_events(self.bucket_id, limit=10)))
         """The recent events we have posted to the aw-watcher-ask-away bucket.
 
         This is used to avoid asking the user to log an absence that they have already logged."""
 
         self.afk_bucket_id = find_afk_bucket(self._all_buckets)
 
-
     @cached_property
     def _all_buckets(self):
         return self.client.get_buckets()
 
-    def has_event(self, event: aw_core.Event):
-        return (event.timestamp, event.duration) in self.recent_events
+    def has_event(self, new: aw_core.Event, overlap_thresh: float = 0.95) -> bool:
+        """Check whether we have already posted an event that overlaps with the new event.
+
+        The self.recent_events data structure used to be a dictionary with keys as timestamp/durration.
+        This method merely checked to see if the new event's (timestamp, durration) tuple was in the dictionary.
+
+        However, for some reason the events coming from the aw-server seem to be slightly inconsistent at times.
+        For example, look at the logs below:
+
+            2023-09-23 19:33:45 [DEBUG]: Got events from the server: [('2023-09-23T23:33:37.730000+00:00', 'not-afk'), ...]
+            2023-09-23 19:33:58 [DEBUG]: Got events from the server: [('2023-09-23T23:33:37.730000+00:00', 'not-afk'), ('2023-09-23T23:33:37.729000+00:00', 'not-afk'), ...]
+
+        The second query returns an overlapping 'not-afk' event with a slightly earlier timestamp.
+        This duplication + offset combination was causing us to double ask the user for input.
+        Using overlaps with a percentage is more robust against this kind of thing.
+        """  # noqa: E501
+        for recent in self.recent_events:
+            overlap_start = max(recent.timestamp, new.timestamp)
+            overlap_end = min(recent.timestamp + recent.duration, new.timestamp + new.duration)
+            overlap = overlap_end - overlap_start
+            if overlap / new.duration > overlap_thresh:
+                return True
+        return False
 
     def post_event(self, event: aw_core.Event, message: str):
         assert not self.has_event(event)  # noqa: S101
         event.data["message"] = message
         event["id"] = None  # Wipe the ID so we don't edit the AFK event.
-        self.recent_events[(event.timestamp, event.duration)] = event
+        logger.debug(f"Posting event: {event}")
+        self.recent_events.append(event)
         self.client.insert_event(self.bucket_id, event)
 
 
@@ -88,6 +109,8 @@ class AWAskAwayState:
         """Check whether we recently finished a large AFK event."""
         try:
             events = self.client.get_events(self.afk_bucket_id, limit=10)
+            events_log = [(e.timestamp.astimezone(LOCAL_TIMEZONE).isoformat(), e.data["status"]) for e in events]
+            logger.debug(f"Got events from the server: {events_log}")
         except HTTPError:
             logger.exception("Failed to get events from the server.")
             return
@@ -101,11 +124,14 @@ class AWAskAwayState:
         pseudo_afk_events = list(get_gaps(non_afk_events))
 
         # Merge close events. This sounds like a good idea but I haven't tested to see if it is really needed.
+        logger.debug(f"Events before heartbeat_reduce: {len(pseudo_afk_events)}")
         pseudo_afk_events = aw_transform.heartbeat_reduce(pseudo_afk_events, pulsetime=10)
+        logger.debug(f"Events after heartbeat_reduce: {len(pseudo_afk_events)}")
         pseudo_afk_events = [e for e in pseudo_afk_events if not self.has_event(e)]
         buffered_now = get_utc_now() - datetime.timedelta(seconds=seconds)
         for event in pseudo_afk_events:
             long_enough = event.duration.seconds > durration_thresh
             recent_enough = event.timestamp + event.duration > buffered_now
             if long_enough and recent_enough:
+                logger.debug(f"Found event to note: {event}")
                 yield event
